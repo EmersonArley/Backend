14b6830121ccf711bd590503971db135762ce193
Emerson <merson352@gmail.com>
2022-02-21 12:26:23 -0500

    21-2-22

--- Appi.js
+++ Appi.js
@@ -1,14 +1,15 @@
-const express= require('express');
-const mysql= require('mysql');
-const bodyparser= require('body-parser');
+const express = require('express');
+const mysql = require('mysql');
+const bodyparser = require('body-parser');
+const cors = require('cors');
 const jwt = require("jsonwebtoken");
 const JWT_Secret = 'claveSecreta';
 const Connection = require('mysql/lib/Connection');
 const port = process.env.port || 3050;
-const app= express();
+const app = express();
+app.use(cors());
 app.use(bodyparser.json());
-app.use(function(res, res, next)
-{
+app.use(function (res, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
@@ -17,109 +18,141 @@ app.use(function(res, res, next)
 })
 
 //myslq
-const conexion= mysql.createConnection({
+const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'test'
 })
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Bienvenidos");
 });
 
 //Listado de usuarios
-app.get('/usuarios', (req, res)=>{
+app.get('/usuarios', (req, res) => {
 
-    const sql= 'SELECT * FROM usuarios';
-    conexion.query(sql, (error, resultado)=>{
+    const sql = 'SELECT * FROM usuarios';
+    conexion.query(sql, (error, resultado) => {
         if (error) throw error;
-        if (resultado.length > 0){
+        if (resultado.length > 0) {
             res.json(resultado)
-        } else{
+        } else {
             res.send("No tenemos resultados");
         }
-        
+
     })
 });
 
 //obtener usuario
-app.get('/usuarios/:id', (req, res)=>{
-    const {id }= req.params
-    const sql= `SELECT * FROM usuarios WHERE id_Usuario= ${id}`
-    conexion.query(sql, (error, resultado)=>{
+app.get('/usuarios/:id', (req, res) => {
+    const { id } = req.params
+    const sql = `SELECT * FROM usuarios WHERE id_Usuario= ${id}`
+    conexion.query(sql, (error, resultado) => {
         if (error) throw error;
-        if (resultado.length > 0){
+        if (resultado.length > 0) {
             res.json(resultado)
-        } else{
+        } else {
             res.send("No tenemos resultados");
         }
-        
+
     })
 });
 
 //Registro de usuarios
-app.post('/registrar', (req, res)=>{
-    const sql= 'INSERT INTO usuarios SET ?';
+app.post('/registrar', (req, res) => {
+    const sql = 'INSERT INTO usuarios SET ?';
     const customerObj = {
         nombreUsuario: req.body.nombreUsuario,
         contrasena: req.body.contrasena
     }
 
-    conexion.query(sql,customerObj, error =>{
-        if(error) throw error;
+    conexion.query(sql, customerObj, error => {
+        if (error) throw error;
         res.send(true);
     });
 });
 
 //Validar Usuario
-app.post('/login',  (req, res) => {
-    const {nombreUsuario, contrasena}= req.body;
+app.post('/login', (req, res) => {
+    const { nombreUsuario, contrasena } = req.body;
 
-        
-            const sql = `SELECT * FROM usuarios WHERE nombreUsuario = '${nombreUsuario}' AND contrasena= '${contrasena}'`;
-            conexion.query(sql, (error, result) => {
-        
-                if(result.length > 0){
-    
-                    
-                   res.send(true);
-                   
-                    
-                } else{
-                    res.send(false);
-                    
+
+    const sql = `SELECT * FROM usuarios WHERE nombreUsuario = '${nombreUsuario}' AND contrasena= '${contrasena}'`;
+    conexion.query(sql, (error, result) => {
+
+        if (result.length > 0) {
+            jwt.sign(
+                { user: result },
+                "secretKey",
+                { expiresIn: "1d" },
+                function (err, token) {
+                  if (err) {
+                    res.send(false)
+                  } else {
+                    res.json (token);
+                  }
                 }
-                
-            })
-    
-    
-  });
+              );
+
+
+        } else {
+            res.send(false);
+
+        }
+
+    })
+
+
+});
+
+//Autorización del token
+
+app.post("/post", verifyToken, (req, res) => {
+    jwt.verify(req.token, "secretKey", (error, authData) => {
+        if (error) {
+            res.send(false);
+        } else {
+            res.send(true);
+        }
+    });
+});
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+        const bearerToken = bearerHeader.split(" ")[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 
 //Actualizar usuarios
-app.put('/actualizar/:id', (req, res)=>{
-    const {id } = req.params;
-    const {nombreUsuario, contrasena}= req.body;
-    const sql= `UPDATE usuarios SET nombreUsuario= '${nombreUsuario}', contrasena = '${contrasena}' WHERE id_Usuario= ${id}`;
+app.put('/actualizar/:id', (req, res) => {
+    const { id } = req.params;
+    const { nombreUsuario, contrasena } = req.body;
+    const sql = `UPDATE usuarios SET nombreUsuario= '${nombreUsuario}', contrasena = '${contrasena}' WHERE id_Usuario= ${id}`;
 
-    conexion.query(sql, error =>{
-        if(error) throw error;
+    conexion.query(sql, error => {
+        if (error) throw error;
         res.send(true);
-        
+
     });
 });
 
 //Borrar
 
-app.delete('/borrar/:id' , (req,res) =>{
-    const {id }= req.params;
+app.delete('/borrar/:id', (req, res) => {
+    const { id } = req.params;
     const sql = `DELETE FROM usuarios WHERE id_Usuario= ${id}`;
-    conexion.query(sql, error =>{
-        if(error) throw error;
+    conexion.query(sql, error => {
+        if (error) throw error;
         res.send(true);
+    });
 });
- });
 
 //Validar conexion
 conexion.connect(error => {
---
+++ test.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-02-2022 a las 15:01:10
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_Usuario` int(11) NOT NULL,
+  `nombreUsuario` varchar(50) NOT NULL,
+  `contrasena` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_Usuario`, `nombreUsuario`, `contrasena`) VALUES
+(16, 'hola', 'kk'),
+(17, 'hola', '11111'),
+(22, 'Vargucci', '1221'),
+(23, '12qasa', 'sasas');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_Usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
